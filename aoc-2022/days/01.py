f3a69096afe01b5e58bafa224bdfdcaf44cf3987
Toni Lastre <tonilastre@gmail.com>
2022-12-01 10:09:04 +0100

    Add day 1

--- aoc-2022/days/01.py
+++ aoc-2022/days/01.py
@@ -1,12 +1,26 @@
 from aoc import run
 
+def iter_number_batches(lines):
+    batch = []
+    for line in lines:
+        if line:
+            batch.append(int(line))
+            continue
+
+        if batch:
+            yield batch
+            batch = []
+
+    if batch:
+        yield batch
+
 def get_first(lines):
-    # Add solution here...
-    return 0
+    sums = (sum(batch) for batch in iter_number_batches(lines))
+    return max(sums)
 
 def get_second(lines):
-    # Add solution here...
-    return 0
+    sums = (sum(batch) for batch in iter_number_batches(lines))
+    return sum(sorted(sums, reverse=True)[:3])
 
 if __name__ == '__main__':
     run(get_first, get_second)
